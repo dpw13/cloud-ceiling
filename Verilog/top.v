1fb29a498a2193475d79a2edd5369b91e374810b
Dane Wagner <dane.wagner@gmail.com>
2022-12-02 16:37:57 -0600

    Instantiate full LED string

--- Verilog/top.v
+++ Verilog/top.v
@@ -265,7 +265,7 @@ module top(
 	// String drivers
 	parallel_strings #(
 		.N_STRINGS(4),
-		.N_LEDS_PER_STRING(128),
+		.N_LEDS_PER_STRING(150),
 		.FIFO_ADDR_WIDTH(FIFO_ADDR_WIDTH),
 		.FIFO_DATA_WIDTH(FIFO_DATA_WIDTH)
 	) all_strings (
