94b624c8f57424f01e49d6202dfaa68f05b92215
Dane Wagner <dane.wagner@gmail.com>
2022-12-02 15:15:39 -0600

    Call ioctl from demo script

--- scripts/demo.py
+++ scripts/demo.py
@@ -2,6 +2,7 @@ import mmap
 import os
 import numpy as np
 import time
+import fcntl
 
 FPGA_REGS_BASE = 0x1000000
 FPGA_REGS_SIZE = 0x2000
@@ -18,11 +19,11 @@ FIFO_DATA_REGION = 0x1001000
 FIFO_DATA_SIZE = 0x1000
 
 LED_COUNT = 128
-STRING_COUNT = 2
+STRING_COUNT = 4
 BYTES_PER_LED = 3
 
 FRAME_SIZE = LED_COUNT*STRING_COUNT*BYTES_PER_LED
-FRAME_WORDS = FRAME_SIZE >> 1
+FRAME_WORDS = FRAME_SIZE >> 2
 
 BLUE = 0
 GREEN = 1
@@ -40,54 +41,50 @@ print(f"Initial empty count is {empty}")
 
 #regs[SCRATCH_REG] = 0x9999
 
-fb_raw = mmap.mmap(memmap, length=FIFO_DATA_REGION, offset=FIFO_DATA_SIZE)
+ledfb_fd = os.open("/dev/ledfb", os.O_RDWR)
+fb_raw = mmap.mmap(ledfb_fd, length=FRAME_SIZE, offset=0)
 # Use numpy to access memory in individual bytes. Note that we only
 # reserve the frame buffer itself.
-fb = np.frombuffer(fb_raw, np.uint16, FRAME_WORDS)
+fb = np.frombuffer(fb_raw, np.uint8, FRAME_SIZE)
+fb_32 = np.frombuffer(fb, np.uint32, FRAME_WORDS)
 
-fb_soft = np.zeros(FRAME_SIZE, dtype=np.uint8)
-fb_soft_16 = np.frombuffer(fb_soft, np.uint16, FRAME_WORDS)
-
-for i in range(0, 256):
+for frame in range(0, 16):
+    #print("Filling")
+    fb_32.fill(0)
     for string_idx in range(0, STRING_COUNT):
         for led_idx in range(0, LED_COUNT):
             # Buffer format is (Pixel/String/Color)
             # 0/0/B, 0/0/R, 0/0/G, 0/1/B, 0/1/R, 0/1/G, ...
             # Base index of Pixel/String is 3*Y + STRING_COUNT*3*X = 3*(Y + STRING_COUNT*X)
-            byte_idx = 3*(string_idx + STRING_COUNT*led_idx)
+            if (led_idx + string_idx + 4*frame) % 8 == 0:
+                byte_idx = 3*(string_idx + STRING_COUNT*led_idx)
 
-            x = (i + led_idx) % 32
-            y = (x + 8) % 32
-            z = (y + 8) % 32
-            fb_soft[byte_idx + BLUE] = x
-            fb_soft[byte_idx + GREEN] = y
-            fb_soft[byte_idx + RED] = z
+                fb[byte_idx + BLUE] = 0x20
+                fb[byte_idx + GREEN] = 0x20
+                fb[byte_idx + RED] = 0x20
 
     empty = regs[FIFO_EMPTY_COUNT_REG]
     while (empty < 2048):
         time.sleep(0.001)
         empty = regs[FIFO_EMPTY_COUNT_REG]
-    #print(f"Empty count before frame {i} is {empty}")
+    #print(f"Empty count before frame {frame} is {empty}")
 
-    for p in range(0, FRAME_WORDS):
-        #print(f"Word {p} = 0x{fb_soft_16[p]:04x}")
-        regs[0x800] = fb_soft_16[p]
-    #np.copyto(fb, fb_soft_16, casting='no')
-
-    empty = regs[FIFO_EMPTY_COUNT_REG]
-    #print(f"Empty count after frame {i} is {empty}")
-
-    #time.sleep(0.01)
+    fcntl.ioctl(ledfb_fd, 0, FRAME_SIZE)
+    time.sleep(0.004)
+    status = regs[FIFO_STATUS_REG]
 
 time.sleep(0.1)
 
 # Turn off LEDs
-for p in range(0, FRAME_WORDS):
-    regs[0x800] = 0
-
+fb_32.fill(0)
+fcntl.ioctl(ledfb_fd, 0, FRAME_SIZE)
+time.sleep(0.004)
 status = regs[FIFO_STATUS_REG]
-status = regs[FIFO_STATUS_REG]
+empty = regs[FIFO_EMPTY_COUNT_REG]
 
-print(f"FIFO status: 0x{status:04x}")
+print(f"FIFO status: 0x{status:04x} empty: {empty}")
 
+fb_raw.close()
+os.close(ledfb_fd)
+regs_raw.close()
 os.close(memmap)
