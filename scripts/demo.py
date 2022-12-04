9be140c60124ef41c76b00908fc7fc47a0e4fd93
Dane Wagner <dane.wagner@gmail.com>
2022-12-04 12:36:51 -0600

    Implement safe teardown

--- scripts/demo.py
+++ scripts/demo.py
@@ -12,15 +12,15 @@ import animation.flame
 import animation.drops
 
 def get_regs(fname="/dev/mem"):
-    memmap = os.open(fname, os.O_RDWR)
-    regs_raw = mmap.mmap(memmap, length=FPGA_REGS_SIZE, offset=FPGA_REGS_BASE)
+    memmap_fd = os.open(fname, os.O_RDWR)
+    regs_raw = mmap.mmap(memmap_fd, length=FPGA_REGS_SIZE, offset=FPGA_REGS_BASE)
     # Use numpy to access memory in 16-bit words
     regs = np.frombuffer(regs_raw, np.uint16, FPGA_REGS_SIZE >> 1)
 
     id_reg = regs[ID_REG]
     print(f"Initialized. ID = 0x{id_reg:04x}")
 
-    return regs
+    return memmap_fd, regs_raw, regs
 
 def get_fb(fname="/dev/ledfb"):
     ledfb_fd = os.open(fname, os.O_RDWR)
@@ -30,7 +30,7 @@ def get_fb(fname="/dev/ledfb"):
     fb = np.frombuffer(fb_raw, np.uint8, FRAME_SIZE)
     fb_32 = np.frombuffer(fb, np.uint32, FRAME_WORDS)
 
-    return (ledfb_fd, fb, fb_32)
+    return (ledfb_fd, fb_raw, fb, fb_32)
 
 parser = argparse.ArgumentParser(
                     prog = 'LED Demo')
@@ -39,48 +39,54 @@ parser.add_argument("animation")
 
 args = parser.parse_args()
 
-regs = get_regs()
+memmap_fd, regs_raw, regs = get_regs()
 empty = regs[FIFO_EMPTY_COUNT_REG]
 print(f"Initial empty count is {empty}")
 
-#regs[SCRATCH_REG] = 0x9999
-
-ledfb_fd, fb, fb_32 = get_fb()
+ledfb_fd, fb_raw, fb, fb_32 = get_fb()
 
 anim = getattr(animation, args.animation)
 
 anim.init()
 
-frame = 0
-while True:
-    anim.render(frame, fb, fb_32)
+try:
+    frame = 0
+    while True:
+        anim.render(frame, fb, fb_32)
 
-    # Make sure we have room in the FIFO
-    empty = regs[FIFO_EMPTY_COUNT_REG]
-    while (empty < 2048):
-        time.sleep(0.001)
+        # Make sure we have room in the FIFO
         empty = regs[FIFO_EMPTY_COUNT_REG]
-    #print(f"Empty count before frame {frame} is {empty}")
+        while (empty < 2048):
+            time.sleep(0.001)
+            empty = regs[FIFO_EMPTY_COUNT_REG]
+        #print(f"Empty count before frame {frame} is {empty}")
 
-    # Serialize framebuffer to LEDs
+        # Serialize framebuffer to LEDs
+        fcntl.ioctl(ledfb_fd, 0, FRAME_SIZE)
+        #time.sleep(0.004)
+        #status = regs[FIFO_STATUS_REG]
+
+        frame += 1
+
+except KeyboardInterrupt:
+    print("Exiting...")
+    # Give system to finish its last ioctl
+    time.sleep(0.01)
+
+    # Turn off LEDs
+    fb_32.fill(0)
+    time.sleep(0.01)
     fcntl.ioctl(ledfb_fd, 0, FRAME_SIZE)
-    #time.sleep(0.004)
-    #status = regs[FIFO_STATUS_REG]
+    time.sleep(0.01)
+    status = regs[FIFO_STATUS_REG]
+    time.sleep(0.01)
+    status = regs[FIFO_STATUS_REG]
+    empty = regs[FIFO_EMPTY_COUNT_REG]
 
-    frame += 1
+    print(f"FIFO status: 0x{status:04x} empty: {empty}")
 
-time.sleep(0.1)
-
-# Turn off LEDs
-fb_32.fill(0)
-fcntl.ioctl(ledfb_fd, 0, FRAME_SIZE)
-time.sleep(0.004)
-status = regs[FIFO_STATUS_REG]
-empty = regs[FIFO_EMPTY_COUNT_REG]
-
-print(f"FIFO status: 0x{status:04x} empty: {empty}")
-
-fb_raw.close()
-os.close(ledfb_fd)
-regs_raw.close()
-os.close(memmap)
+finally:
+    fb_raw.close()
+    os.close(ledfb_fd)
+    regs_raw.close()
+    os.close(memmap_fd)
