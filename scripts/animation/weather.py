dd38091e6af099699875f2987bb6b56cbcc65bb6
Dane Wagner <dane.wagner@gmail.com>
2023-01-30 09:42:09 -0600

    Rework sky colors

--- scripts/animation/weather.py
+++ scripts/animation/weather.py
@@ -10,24 +10,24 @@ import wand.drawing
 SKY_BG_MAP = [
     # Format:
     # [ age, r, g, b ]
-    [  0.0, 0x07, 0x0B, 0x34 ], # "Cetacean Blue"
-    [  4.0, 0x07, 0x0B, 0x34 ],
-    [  6.0, 0x48, 0x34, 0x75 ], # "Regalia"
-    [  7.0, 0xD7, 0x79, 0x8B ], # dusty rose
-    [  8.0, 0xFE, 0xE1, 0x97 ], # straw yellow
-    [  8.5, 0xF3, 0xEC, 0xD0 ], # pale cream
-    [  9.0, 0xC6, 0xD0, 0xDE ], # light gray-blue
-    [  9.5, 0x90, 0xA7, 0xC4 ], # medium blue
-    [ 10.0, 0x6B, 0xCF, 0xFF ], # medium blue
-    [ 11.0, 0x00, 0xAD, 0xFF ], # deep day blue
-    [ 16.0, 0x6B, 0xCF, 0xFF ], # medium blue
-    [ 17.0, 0xFF, 0xE3, 0x73 ], # "Shandy"
-    [ 18.0, 0xFC, 0x9C, 0x54 ], # "Sandy Brown"
-    [ 18.5, 0xFD, 0x5E, 0x53 ], # "Sunset Orange"
-    [ 19.0, 0x4B, 0x3D, 0x60 ], # "English Violet"
-    [ 20.0, 0x15, 0x28, 0x52 ], # "Space Cadet"
-    [ 21.0, 0x08, 0x18, 0x3A ], # "Maastricht Blue"
-    [ 24.0, 0x07, 0x0B, 0x34 ], # "Cetacean Blue"
+    [  0.0, 0x01, 0x01, 0x06 ], # "Cetacean Blue"
+    [  4.0, 0x01, 0x02, 0x09 ],
+    [  6.0, 0x07, 0x05, 0x0C ], # "Regalia"
+    [  7.0, 0x10, 0x09, 0x0B ], # dusty rose
+    [  8.0, 0x13, 0x11, 0x0C ], # straw yellow
+    [  8.5, 0x17, 0x16, 0x13 ], # pale cream
+    [  9.0, 0x17, 0x18, 0x19 ], # light gray-blue
+    [  9.5, 0x19, 0x1D, 0x22 ], # medium blue
+    [ 10.0, 0x12, 0x23, 0x2B ], # medium blue
+    [ 11.0, 0x00, 0x27, 0x39 ], # deep day blue
+    [ 16.0, 0x12, 0x23, 0x2B ], # medium blue
+    [ 17.0, 0x1F, 0x1B, 0x0E ], # "Shandy"
+    [ 18.0, 0x21, 0x14, 0x0B ], # "Sandy Brown"
+    [ 18.5, 0x1C, 0x0A, 0x09 ], # "Sunset Orange"
+    [ 19.0, 0x0C, 0x09, 0x0F ], # "English Violet"
+    [ 20.0, 0x04, 0x07, 0x0E ], # "Space Cadet"
+    [ 21.0, 0x01, 0x03, 0x08 ], # "Maastricht Blue"
+    [ 24.0, 0x01, 0x01, 0x06 ], # "Cetacean Blue"
 ]
 
 TIME_XP = np.array([m[0] for m in SKY_BG_MAP])
@@ -46,7 +46,7 @@ def init():
 
 def render(frame, fb, fb_32):
     with wand.drawing.Drawing() as draw:
-        current_time = (frame / 6) % 24
+        current_time = (frame / 60) % 24
         
         b = int(round(np.interp(current_time, TIME_XP, B_BG)))
         g = int(round(np.interp(current_time, TIME_XP, G_BG)))
