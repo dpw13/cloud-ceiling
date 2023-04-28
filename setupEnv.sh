779d8f7f2d029c604e5687e73e5d95ee1b9ff490
Dane Wagner <dane.wagner@gmail.com>
2023-04-28 15:52:58 -0500

    Add rust toolchain to setupenv

--- setupEnv.sh
+++ setupEnv.sh
@@ -3,3 +3,6 @@
 export MODELSIM=/opt/lscc/iCEcube2.2020.12/modeltech
 export PATH=$PATH:$MODELSIM/linuxloem
 export LM_LICENSE_FILE=/opt/lattice_icecube2.dat
+
+export PKG_CONFIG_SYSROOT_DIR=/home/dwagner/Documents/sysroots/beaglebone
+export PATH=$PATH:/home/dwagner/x-tools/arm-unknown-linux-gnueabihf/bin
