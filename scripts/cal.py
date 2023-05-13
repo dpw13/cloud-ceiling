38bf74797f7150e9bb7ebfed540f31d5c1f7a012
Dane Wagner <dane.wagner@gmail.com>
2023-05-13 14:12:37 -0500

    Cal fixup

--- scripts/cal.py
+++ scripts/cal.py
@@ -5,14 +5,15 @@ import colour
 import json
 import time
 
-DISPLAYCAL_URI = "http://192.168.1.139:8080/ajax/messages"
+DISPLAYCAL_URI = "http://localhost:8080/ajax/messages"
 FB_URI = "http://beaglebone:3000/set_config"
 
 last_color = colour.Color("grey")
 
 while True:
     r = requests.get(DISPLAYCAL_URI, data=last_color.hex_l)
-    f = r.split(" ")
+    print(f"Got color {r.text}\n")
+    f = r.text.split(" ")
     c = colour.Color(rgb=f)
     if last_color != c:
         last_color = c
