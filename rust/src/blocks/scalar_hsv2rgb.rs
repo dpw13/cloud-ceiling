40a6293ac1d6e9b46350d75a09847fd78b6903a7
Dane Wagner <dane.wagner@gmail.com>
2024-01-06 14:03:25 -0600

    Fix HSV value calculation

--- rust/src/blocks/scalar_hsv2rgb.rs
+++ rust/src/blocks/scalar_hsv2rgb.rs
@@ -61,6 +61,7 @@ impl RenderBlock for ScalarHsv2Rgb {
     fn execute(&mut self, state: &mut RenderState) {
         let v = state.get_scalar(self.v_idx).clamp(0.0, 1.0);
         let c = v * state.get_scalar(self.s_idx).clamp(0.0, 1.0);
+        let m = v - c;
 
         // Typically H is 0 to 360 degrees. Here we use the range 0.0 to 1.0. To get
         // H / 60 deg, we just multiply H by 6.0. Call this hp (H') to clarify that
@@ -71,12 +72,12 @@ impl RenderBlock for ScalarHsv2Rgb {
         let sector = hp.floor().rem_euclid(6.0) as u8;
 
         let rcolor = match sector {
-            0 => RealColor { r: c, g: x, b: 0.0 },
-            1 => RealColor { r: x, g: c, b: 0.0 },
-            2 => RealColor { r: 0.0, g: c, b: x },
-            3 => RealColor { r: 0.0, g: x, b: c },
-            4 => RealColor { r: x, g: 0.0, b: c },
-            5 => RealColor { r: c, g: 0.0, b: x },
+            0 => RealColor { r: c + m, g: x + m, b: m },
+            1 => RealColor { r: x + m, g: c + m, b: m },
+            2 => RealColor { r: m, g: c + m, b: x + m },
+            3 => RealColor { r: m, g: x + m, b: c + m },
+            4 => RealColor { r: x + m, g: m, b: c + m },
+            5 => RealColor { r: c + m, g: m, b: x + m },
             x => panic!("Invalid HSV sector {x}"),
         };
 
