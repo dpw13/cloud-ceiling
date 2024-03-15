844166f1ce15eb3da00bd77372335aebeef97813
Dane Wagner <dane.wagner@gmail.com>
2024-03-15 23:38:10 +0000

    Fix up mic light control

--- scripts/mic_lights.py
+++ scripts/mic_lights.py
@@ -13,7 +13,7 @@ def get_regs(fname="/dev/mem"):
     memmap_fd = os.open(fname, os.O_RDWR)
     regs_raw = mmap.mmap(memmap_fd, length=FPGA_REGS_SIZE, offset=FPGA_REGS_BASE)
     # Use numpy to access memory in 32-bit words
-    regs = np.frombuffer(regs_raw, np.uint32, FPGA_REGS_SIZE >> 1)
+    regs = np.frombuffer(regs_raw, np.uint32, FPGA_REGS_SIZE >> 2)
 
     id_reg = regs[ID_REG]
     print(f"Initialized. ID = 0x{id_reg:04x}")
@@ -30,14 +30,16 @@ parser.add_argument("-i", "--intensity", type=int, default=16)
 parser.add_argument("-c", "--color", type=auto_int, default=16)
 
 args = parser.parse_args()
-args.set_args(args)
 
 memmap_fd, regs_raw, regs = get_regs()
 
-led_data = 0xE000000 | (args.intensity << 24) | args.color
+led_data = 0xE0000000 | (args.intensity << 24) | args.color
+print(f"LED data word {led_data:x}")
 
 # See SK9822 documentation
 regs[MIC_LED_DATA_REG] = 0 # frame start
+#time.sleep(0.001)
 for i in range(0, 12):
     regs[MIC_LED_DATA_REG] = led_data
+    #time.sleep(0.001)
 regs[MIC_LED_DATA_REG] = 0xFFFFFFFF # frame end
