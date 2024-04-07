037af45efcab29f76b7a900e7fe6fb3a50a7dbd7
Dane Wagner <dane.wagner@gmail.com>
2024-04-07 15:34:02 -0500

    Add argparser to set_white script

--- scripts/set_white.py
+++ scripts/set_white.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
+import argparse
 import logging
 import requests
 import json
-import sys
 
 logging.basicConfig(level=logging.INFO)
 
@@ -11,8 +11,16 @@ session = requests.Session()
 
 LED_URI = "http://beaglebone:3000/set_white_led"
 
+parser = argparse.ArgumentParser(prog="set_white.py", description="Directly set white LED values")
+
+parser.add_argument("-t", "--temp", type=int, default=4700)
+parser.add_argument("-v", "--value", type=float, default=0.5)
+parser.add_argument("-d", "--delay", type=float, default=0.5)
+
+args = parser.parse_args()
+
 # Set color
 session.post(LED_URI, json.dumps({
-    "temp": float(sys.argv[1]),
-    "value": float(sys.argv[2]),
-    "delay": 2.0}))
+    "temp": args.temp,
+    "value": args.value,
+    "delay": args.delay}))
